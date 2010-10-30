cb655bea09b54d99af9b396534de06997c606884
이덕준 <gochist@gmail.com>
2010-10-31 00:23:49 +0900

    api urls will be commented out until auth module can be run.

--- apiprj/urls.py
+++ apiprj/urls.py
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 
-    (r'^1/', include('api1.urls')),
-    (r'^', include('api1.urls')),
+    #(r'^1/', include('api1.urls')),
+    #(r'^', include('api1.urls')),
 )
