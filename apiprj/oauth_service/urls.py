b9d4615af7712f94b1fb485000abb21ffbb74fc7
이덕준 <gochist@gmail.com>
2010-11-18 15:52:05 +0900

    add skeleton urls for oauth

--- apiprj/oauth_service/urls.py
+++ apiprj/oauth_service/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('oauth_service.views',
     (r'^request_token', 'request_token'),
+    (r'^access_token', 'access_token'),
+    (r'^authorize', 'authorize'),
 )
--- apiprj/oauth_service/views.py
+++ apiprj/oauth_service/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 def request_token(request):
     ret = ""
     return HttpResponse(ret)
+
+def access_token(request):
+    ret = ""
+    return HttpResponse(ret)
+
+def authorize(request):
+    ret = ""
+    return HttpResponse(ret)
+
