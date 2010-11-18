bc09efff3352fa5363d294fa36d8004e904154f4
이덕준 <gochist@gmail.com>
2010-11-18 17:46:50 +0900

    oauth request token implement

--- apiprj/oauth_service/views.py
+++ apiprj/oauth_service/views.py
@@ -1,11 +1,40 @@
 from django.http import HttpResponse
 from api1.models import Member
 from models import *
+import oauth2 as oauth
 
+def initialize_server_request(request):
+    auth_header = {}
+    if request.META.has_key('Authorization'):
+        auth_header['Authorization'] = request.META['Authorization']
+    elif request.META.has_key('HTTP_AUTHORIZATION'):
+        auth_header['Authorization'] = request.META['HTTP_AUTHORIZATION']
+
+    parameters = {}
+    if request.method == "POST" and\
+       request.META['CONTENT_TYPE'] == "applicaion/x-www-form-urlencoded":
+        parameters = dict(request.REQUEST.items())
+
+    query_string = request.META.get('QUREY_STRING','')
+    oauth_request = oauth.Request.from_request(request.method,
+                                               request.build_absolute_uri(),
+                                               headers=auth_header,
+                                               parameters=parameters,
+                                               query_string=query_string)
+
+    if oauth_request:
+        oauth_server = oauth.Server()
+        oauth_server.add_signature_method(oauth.SignatureMethod_HMAC_SHA1()) 
+    else: 
+        oauth_server = None
+    
+    return oauth_server, oauth_request
+  
 def request_token(request):
-    print request.GET['oauth_consumer_key']
-    ret = "oauth_token=foo&oauth_token_secret=bar"
-    return HttpResponse(ret)
+    oauth_server, oauth_request = initialize_server_request(request)
+    # TODO: make token here!!!
+    token = "psuedo token"
+    return HttpResponse(token)
 
 def access_token(request):
     ret = ""
