b57457ea55fdb4f0e7a30722d549d18492a27b8b
이덕준 <gochist@gmail.com>
2011-02-13 09:52:20 +0900

    pretty formatting

--- apiprj/api1_app/doc_views.py
+++ apiprj/api1_app/doc_views.py
@@ -1,10 +1,13 @@
-#! -*- coding: utf8 -*- 
+# -*- coding: utf8 -*-
+""" API Reference 를 view.py 의 주석으로 자동 생성. """ 
 from django.core import urlresolvers
 from django.contrib.admindocs import utils
-from django.contrib.admindocs.views import (get_root_path,
-                                            missing_docutils_page,
-                                            extract_views_from_urlpatterns,
-                                            simplify_regex)
+from django.contrib.admindocs.views import (
+    get_root_path,
+    missing_docutils_page,
+    extract_views_from_urlpatterns,
+    simplify_regex
+)
 from django.http import Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -22,9 +25,12 @@ def view_index(request):
                      extract_views_from_urlpatterns(oauth_urls.urlpatterns)
 
     for (func, regex) in view_functions:
-        view = {'name': getattr(func, '__name__', func.__class__.__name__),
-                'module': func.__module__,
-                'url': simplify_regex(regex)}
+        view = {
+            'name': getattr(func, '__name__', func.__class__.__name__),
+            'module': func.__module__,
+            'url': simplify_regex(regex)
+        }
+
         if view['name'] in ['authorize', 'access_token', 'request_token']:
             view['url'] = "/oauth%s" % view['url']
             view['group'] = 'oauth'
@@ -37,10 +43,13 @@ def view_index(request):
         else:
             view_dict[view['group']] = [view]
     
-    return render_to_response('doc/view_index.tpl', {
-        'root_path': get_root_path(),
-        'view_dict': view_dict
-    }, context_instance=RequestContext(request))
+    return render_to_response(
+        'doc/view_index.tpl', {
+            'root_path': get_root_path(),
+            'view_dict': view_dict
+        }, 
+        context_instance=RequestContext(request)
+    )
     
 def view_detail(request, view):
     if not utils.docutils_is_available:
@@ -57,11 +66,19 @@ def view_detail(request, view):
     if body:
         body = utils.parse_rst(body, 'view', _('view:') + view)
     for key in metadata:
-        metadata[key] = utils.parse_rst(metadata[key], 'model', _('view:') + view)
+        metadata[key] = utils.parse_rst(
+            metadata[key], 
+            'model', _('view:') + view
+        )
     
     name = view.split('.')[-1].replace('_', '/')
-    param = {'root_path': get_root_path(), 'name': name, 'summary': title,
-             'body': body, 'meta': metadata}
+    param = {
+        'root_path': get_root_path(), 
+        'name': name, 
+        'summary': title,
+        'body': body, 
+        'meta': metadata
+    }
     
     return render_to_response('doc/view_detail.tpl', param,
                               context_instance=RequestContext(request))
